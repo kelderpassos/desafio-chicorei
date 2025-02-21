cee97d769d29bde2f5e557dba74742623d24e811
kelderpassos <kelder.passos@gmail.com>
2025-02-21 12:05:16 -0300

    feat: adc security group para EC2 e RDS

--- exercicio-1/vpc.tf
+++ exercicio-1/vpc.tf
@@ -1,3 +1,4 @@
+# vpc
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
   enable_dns_support = "true"
@@ -10,13 +11,14 @@ resource "aws_vpc" "main" {
   }
 }
 
+# subnet
 resource "aws_subnet" "public" {
   vpc_id = aws_vpc.main.id
   cidr_block = var.public_subnet
-  availability_zone = var.az1
+  availability_zone = local.az1
 
   tags = {
-    Name = "${var.project_name}-public-subnet-1"
+    Name = "${var.project_name}-public-subnet"
     subnet_type = "public"
     created_at = timestamp()
   }
@@ -25,7 +27,73 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   vpc_id = aws_vpc.main.id
   cidr_block = var.private_subnet
-  availability_zone = var.az1
+  availability_zone = local.az1
+
+  tags = {
+    Name = "${var.project_name}-private-subnet"
+    subnet_type = "private"
+    created_at = timestamp()
+  }
+}
+
+# security group
+resource "aws_security_group" "vpc" {
+  name = "main_vpc_security_group"
+  vpc_id = aws_vpc.main.id
+  description = "Habilita trafego de entrada e saida para a VPC"
+
+  tags = {
+    Name = "${var.project_name}-sg"
+    created_at = timestamp()
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.vpc.id
+  description = "Habilita trafego de entrada SSH"
+  ip_protocol = "tcp"
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 22
+  to_port = 22
+
+  tags = {
+    Name = "${var.project_name}-allow_ssh"
+    created_at = timestamp()
+  }    
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.vpc.id
+  description = "Habilita trafego de entrada HTTPS"
+  ip_protocol = "tcp"
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 443
+  to_port = 443
+
+  tags = {
+    Name = "${var.project_name}-allow_ssh"
+    created_at = timestamp()
+  }    
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_https" {
+  security_group_id = aws_security_group.vpc.id
+  description = "Habilita todo trafego de saida"
+  ip_protocol = "tcp"
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 443
+  to_port = 433
+
+  tags = {
+    Name = "${var.project_name}-allow_all"
+    created_at = timestamp()
+  }
+}
+
+
+# RDS
+resource "aws_db_subnet_group" "rds_subnet_grp" {
+  subnet_ids = ["${aws_subnet.private.id}"]
 
   tags = {
     Name = "${var.project_name}-private-subnet-2"
@@ -34,7 +102,38 @@ resource "aws_subnet" "private" {
   }
 }
 
+resource "aws_security_group" "rds_sg" {
+  name = "rds_vpc_security_group"
+  vpc_id = aws_vpc.main.id
+  description = "Habilita trafego do EC2 p/ RDS"
 
+  tags = {
+    Name = "${var.project_name}-sg-rds"
+    created_at = timestamp()
+  }
+}
 
+resource "aws_vpc_security_group_ingress_rule" "rds_ingress_rule" {
+  security_group_id = aws_security_group.rds_sg.id
+  description = "Habilita trafego de entrada p/ RDS"
+  ip_protocol = "tcp"
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 3306
+  to_port = 3306
+}
+
+resource "aws_vpc_security_group_egress_rule" "rds_egress" {
+  security_group_id = aws_security_group.rds_sg.id
+  description = "Habilita todo trafego de saida"
+  ip_protocol = -1
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 0
+  to_port = 0
+
+  tags = {
+    Name = "${var.project_name}-rds-egress"
+    created_at = timestamp()
+  }
+}
 
 
