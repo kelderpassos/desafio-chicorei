379f2f6ee3f7b8b3fac0138896ad91a8f31adc5f
kelderpassos <kelder.passos@gmail.com>
2025-02-21 13:19:17 -0300

    feat: adc instancia ec2

--- exercicio-1/instances.tf
+++ exercicio-1/instances.tf
@@ -1,9 +1,35 @@
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+resource "aws_key_pair" "terraform_key" {
+  key_name = var.key_name
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "aws_instance" "ec2_instance" {
+  depends_on = [ aws_db_instance.wordpress_db ]
+
+  ami = var.ami
+  instance_type = var.ec2_instance_type
+  subnet_id = aws_subnet.public.id
+  vpc_security_group_ids = [ "${aws_security_group.ec2_sg.id}" ]
+  associate_public_ip_address = true
+  key_name = aws_key_pair.terraform_key
+
+  tags = {
+    Name = "${var.project_name}-instance"
+    created_at = timestamp()
+  }
+}
+
 resource "aws_db_instance" "rds_instance" {
   allocated_storage    = 10
   storage_type         = "gp2"
   engine               = "mysql"
   engine_version       = "8.0"
-  instance_class       = var.rds_instance
+  instance_class       = var.rds_instance_type
   identifier           = "${var.project_name}-rds"
   db_name              = local.secrets.db_name
   username             = local.secrets.db_username
