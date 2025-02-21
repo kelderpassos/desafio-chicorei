f24f857b2e146a81d443b2844884de03b99c27b5
kelderpassos <kelder.passos@gmail.com>
2025-02-21 14:17:04 -0300

    feat: adc variavel para bucket s3

--- exercicio-1/variables.tf
+++ exercicio-1/variables.tf
@@ -11,6 +11,9 @@ variable "ec2_instance_type" {}
 variable "key_name" {}
 variable "rds_instance_type" {}
 
+# s3
+variable "bucket_name" {}
+
 # vpc
 variable "allowed_ssh_ip" {}
 variable "public_subnet" {}
