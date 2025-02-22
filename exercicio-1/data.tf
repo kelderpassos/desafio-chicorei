4e09c0ce820551bd36d6f03a99b97d99adf206b4
kelderpassos <kelder.passos@gmail.com>
2025-02-22 06:23:32 -0300

    feat: adc data resource para buscar certificado ssl para o alb

--- exercicio-1/data.tf
+++ exercicio-1/data.tf
@@ -4,3 +4,9 @@ data "aws_availability_zones" "azs" {}
 data "aws_secretsmanager_secret_version" "rds-secrets" {
   secret_id = var.rds_secrets
 }
+
+data "aws_acm_certificate" "alb_certificate" {
+  domain   = var.domain_name
+  statuses = ["ISSUED"]
+  most_recent = true
+}
